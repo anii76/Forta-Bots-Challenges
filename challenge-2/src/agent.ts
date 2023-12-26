e8a71e8a3013f4e122db19d9d80bd973ace08548
anii76 <ka_bourouina@esi.dz>
2023-12-26 19:07:40 +0100

    feat: fetch swap events.

--- challenge-2/src/agent.ts
+++ challenge-2/src/agent.ts
@@ -1,27 +1,53 @@
 import {
-  BlockEvent,
   Finding,
-  Initialize,
-  HandleBlock,
-  HealthCheck,
   HandleTransaction,
-  HandleAlert,
-  AlertEvent,
   TransactionEvent,
   FindingSeverity,
   FindingType,
 } from "forta-agent";
 
-const handleTransaction: HandleTransaction = async (
-  txEvent: TransactionEvent
-) => {
-  const findings: Finding[] = [];
+//for a specific pool
+const swap_event : string =
+  "event Swap(address indexed sender, address indexed recipient, int256 amount0, int256 amount1, uint160 sqrtPriceX96, uint128 liquidity, int24 tick)";
+const created_pool_event =
+  "event PoolCreated(address indexed token0, address indexed token1, uint24 indexed fee, int24 tickSpacing, address pool)";
+const uniswap_router = "0x3fC91A3afd70395Cd496C647d5a6CC9D4B2b7FAD"; //ethereum (Universal router ?)
+const swapv3_router : string = "0xE592427A0AEce92De3Edee1F18E0157C05861564"; //ethereum / Polygone
+const pools = "";
 
- ///
+function provideHandleTransaction(
+  swap_event: string,
+  swapv3_router : string
+): HandleTransaction {
+  return async function handleTransaction(txEvent: TransactionEvent) {
+    const findings: Finding[] = [];
 
-  return findings;
-};
+    //if (txEvent.to != swapv3_router) return findings;
+    console.log("here 1");
+    const swaps =  txEvent.filterLog(swap_event ,swapv3_router.toLocaleLowerCase());
+    console.log("here 2");
+    if (!swaps.length) return findings;
+    console.log("here 3");
+    swaps.forEach((swap) => {
+      //console.log(swap.args.toString());
+      findings.push(
+        Finding.fromObject({
+          name: "Nethermind Swaps Detector",
+          description: `New swap :`,
+          alertId: "Swap",
+          protocol: "uniswapv3",
+          severity: FindingSeverity.Info,
+          type: FindingType.Info,
+          metadata: {},
+        })
+      );
+    });
+      
+
+    return findings;
+  };
+}
 
 export default {
-  handleTransaction,
+  handleTransaction: provideHandleTransaction(swap_event, swapv3_router),
 };
