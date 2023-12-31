b462f187619cbbeadab43b1697eaaee4516b9bf5
anii76 <ka_bourouina@esi.dz>
2023-12-31 20:40:59 +0100

    chore: update findings.ts

--- challenge-2/src/findings.ts
+++ challenge-2/src/findings.ts
@@ -1,13 +1,12 @@
-import { Finding, FindingSeverity, FindingType, LogDescription } from "forta-agent";
+import { Finding, FindingSeverity, FindingType } from "forta-agent";
 
-//change args by event.args and event["address"](poolAddress)
 export const createFinding = (poolAddress: string, args: any): Finding => {
   const { sender, recipient, amount0, amount1, sqrtPriceX96, liquidity, tick } = args;
   return Finding.fromObject({
-    name: "Nethermind Swaps Detector",
-    description: `New swap :`,
-    alertId: "Swap",
-    protocol: "uniswapv3",
+    name: "UniswapV3 Swaps Detector",
+    description: "New swap detected",
+    alertId: "UniswapV3-Swap",
+    protocol: "UniswapV3",
     severity: FindingSeverity.Info,
     type: FindingType.Info,
     metadata: {
@@ -16,9 +15,6 @@ export const createFinding = (poolAddress: string, args: any): Finding => {
       amount1: amount1.toString(),
       sender: sender,
       recipient: recipient,
-      sqrtPriceX96: sqrtPriceX96.toString(),
-      liquidity: liquidity.toString(),
-      tick: tick,
     },
   });
 };
