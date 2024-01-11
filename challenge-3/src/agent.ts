34f57b7efeb13485cc7539ab507269a6cb75dd92
anii76 <ka_bourouina@esi.dz>
2024-01-11 23:46:22 +0100

    feat: initial approach

--- challenge-3/src/agent.ts
+++ challenge-3/src/agent.ts
@@ -3,90 +3,150 @@ import {
   Finding,
   Initialize,
   HandleBlock,
-  HealthCheck,
   HandleTransaction,
   HandleAlert,
   AlertEvent,
   TransactionEvent,
   FindingSeverity,
   FindingType,
+  getEthersProvider,
+  getAlerts,
+  AlertQueryOptions,
 } from "forta-agent";
+import { NetworkManager } from "forta-agent-tools";
+import { providers, BigNumber, Contract } from "ethers";
+import { Interface } from "ethers/lib/utils";
+import { LRUCache } from "lru-cache";
 
-export const ERC20_TRANSFER_EVENT =
-  "event Transfer(address indexed from, address indexed to, uint256 value)";
-export const TETHER_ADDRESS = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
-export const TETHER_DECIMALS = 6;
-let findingsCount = 0;
+const iface = new Interface([
+  "function totalSupply() external view returns (uint256)",
+  "function balanceOf(address account) external view returns (uint256)",
+]);
 
-const handleTransaction: HandleTransaction = async (
-  txEvent: TransactionEvent
-) => {
-  const findings: Finding[] = [];
+//LRU Cache <address(dai)[blocknum?],{balances?} [2 caches , one for L1 second for L2]
+const cacheL1: LRUCache<string, any> = new LRUCache<string, any>({ max: 500 });
+const cacheL2: LRUCache<string, any> = new LRUCache<string, any>({ max: 500 });
 
-  // limiting this agent to emit only 5 findings so that the alert feed is not spammed
-  if (findingsCount >= 5) return findings;
+//-------------------------------------------------------------
+//use multicall to request both token balances in one call
 
-  // filter the transaction logs for Tether transfer events
-  const tetherTransferEvents = txEvent.filterLog(
-    ERC20_TRANSFER_EVENT,
-    TETHER_ADDRESS
-  );
+//using network calls ? can I call both networks or not.(let's try by manually changing the network)
+//or use calls with same infura key
 
-  tetherTransferEvents.forEach((transferEvent) => {
-    // extract transfer event arguments
-    const { to, from, value } = transferEvent.args;
-    // shift decimals of transfer value
-    const normalizedValue = value.div(10 ** TETHER_DECIMALS);
+const ESCROW_ARBITRUM_ADDRESS = "0xA10c7CE4b876998858b1a9E12b10092229539400";
+const ESCROW_OPTIMISM_ADDRESS = "0x467194771dAe2967Aef3ECbEDD3Bf9a310C76C65";
+const DAI_L1_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+const DAI_L2_ADDRESS = "0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1";
 
-    // if more than 10,000 Tether were transferred, report it
-    if (normalizedValue.gt(10000)) {
+const provider = getEthersProvider();
+
+export const provideHandleBlock =
+  (provider: providers.Provider): HandleBlock =>
+  async (blockEvent: BlockEvent) => {
+    const findings: Finding[] = [];
+
+    const { chainId } = await provider.getNetwork();
+
+    if (chainId == 1) {
+      const daiContract = new Contract(DAI_L1_ADDRESS, iface, provider);
+      const escrowBalance1 = BigNumber.from(
+        await daiContract.balanceOf(ESCROW_ARBITRUM_ADDRESS, { blockTag: blockEvent.blockNumber })
+      ).toString();
+      const escrowBalance2 = BigNumber.from(
+        await daiContract.balanceOf(ESCROW_OPTIMISM_ADDRESS, { blockTag: blockEvent.blockNumber })
+      ).toString();
+
       findings.push(
         Finding.fromObject({
-          name: "High Tether Transfer",
-          description: `High amount of USDT transferred: ${normalizedValue}`,
-          alertId: "FORTA-1",
-          severity: FindingSeverity.Low,
+          name: "Escrow Account Balance",
+          description: `Escrow Account balances `,
+          alertId: "FORTA-6",
+          severity: FindingSeverity.Info,
           type: FindingType.Info,
           metadata: {
-            to,
-            from,
+            balanceArbitrum: escrowBalance1.toString(),
+            balanceOptimism: escrowBalance2.toString(),
           },
         })
       );
-      findingsCount++;
+    } else {
+      const daiContract = new Contract(DAI_L2_ADDRESS, iface, provider);
+      const l2DaiSupply = BigNumber.from(
+        await daiContract.totalSupply({ blockTag: blockEvent.blockNumber })
+      ).toString();
+
+      const query: AlertQueryOptions = {
+        botIds: ["0xdb5f76edad8195236876f0ddf13c2e6b3ac807e5b2b6a9d8b7795a8c0fa59f22"],
+        alertId: "FORTA-6",
+        first: 1,
+      };
+      const results = await getAlerts(query);
+
+      results.alerts.forEach((alert) => {
+        const escrowBalance = chainId == 10 ? alert.metadata.balanceArbitrum : alert.metadata.balanceOptimism;
+      });
+
+      findings.push(
+        Finding.fromObject({
+          name: "MakerDOA Invariant violated",
+          description: ``,
+          alertId: "FORTA-7",
+          severity: FindingSeverity.Info,
+          type: FindingType.Info,
+          metadata: {
+            network: blockEvent.network.toString(),
+            l2DaiSupply: l2DaiSupply,
+          },
+        })
+      );
     }
-  });
+    //cache balance ?
 
-  return findings;
-};
+    //get alert now ? I guess then compare balances ? [get previous balance & compare them :)) & if this is the first call (without cache then pass)]
 
-// const initialize: Initialize = async () => {
-//   // do some initialization on startup e.g. fetch data
-// }
+    return findings;
+  };
 
-// const handleBlock: HandleBlock = async (blockEvent: BlockEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some block condition
-//   return findings;
-// }
+/*export const provideHandleAlert =
+  (networkManager: NetworkManager<NetworkData>, provider: providers.Provider): HandleAlert =>
+  async (alertEvent: AlertEvent) => {
+    const findings: Finding[] = [];
 
-// const handleAlert: HandleAlert = async (alertEvent: AlertEvent) => {
-//   const findings: Finding[] = [];
-//   // detect some alert condition
-//   return findings;
-// }
-
-// const healthCheck: HealthCheck = async () => {
-//   const errors: string[] = [];
-  // detect some health check condition
-  // errors.push("not healthy due to some condition")
-  // return errors;
-// }
+    return findings;
+  };*/
 
 export default {
-  // initialize,
-  handleTransaction,
-  // healthCheck,
-  // handleBlock,
-  // handleAlert
+  handleBlock: provideHandleBlock(provider),
+  //handleAlert: provideHandleAlert(networkManager, provider),
 };
+
+/**
+ * 
+interface NetworkData {
+  address: string;
+  num: number;
+}
+
+const data: Record<number, NetworkData> = {
+  // Arbitrum DAI Escrow
+  10: {
+    address: "0xA10c7CE4b876998858b1a9E12b10092229539400",
+    num: 1,
+  },
+  // Optimism DAI Escrow
+  42161: {
+    address: "0x467194771dAe2967Aef3ECbEDD3Bf9a310C76C65",
+    num: 2,
+  },
+};
+const networkManager = new NetworkManager<NetworkData>(data);
+
+const provideInitilize = (networkManager: NetworkManager<NetworkData>, provider: providers.Provider): Initialize => {
+  return async () => {
+    await networkManager.init(provider);
+  };
+};
+
+//networkManager.get("address"); // "address1" if the ChainID is 1, "address42" if the ChainID is 42
+
+ */
